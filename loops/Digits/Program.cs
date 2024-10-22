597d48a3fef605e026cf9ebf0ac0ff90fca0d3c0
Atib Pokharel <atibp1722@gmail.com>
2024-10-22 15:26:57 +0545

    add files related to patterns based programs

---
+++ loops/Digits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("digits sum");
+        Console.WriteLine("----------");
+
+        int num = 72252;
+
+        int rem = 0;
+        int sum = 0;
+        
+        for (int i= 1; i <=5; i++)
+        {
+            if(i==1 || i == 5)
+            {
+                rem = num % 10;
+                sum = sum + rem;
+            }
+            num = num / 10;
+        }
+        Console.WriteLine(sum);
+    }
+}
---
+++ loops/patterns/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("number pattern");
+        Console.WriteLine("--------------");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j=1; j <= 5; j++)
+            {
+                Console.Write(j+" ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine(" ");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine(" ");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.Write(i*j+" ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine(" ");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.Write(i + j + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine(" ");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine(" ");
+
+        for (int i = 5; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine(" ");
+
+        Console.WriteLine("char pattern");
+        Console.WriteLine("------------");
+
+        char chr = 'a';
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(chr+ " ");
+                chr++;
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine(" ");
+
+        char chr1 = 'A';
+        for (int i = 5; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(chr1 + " ");
+                chr1++;
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine(" ");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 5; j >= i; j--)
+            {
+                Console.Write(" ");
+            }
+            for(int k=1; k<=i; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine(" ");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 5; j >= i; j--)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write("*"+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
