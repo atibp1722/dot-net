761ff504db20031e53ed9275476d8113d051197d
Atib Pokharel <atibp1722@gmail.com>
2024-10-24 15:05:04 +0545

    add file related to namespace and exceptions; namespace 'using' keyboard for explicit call and aliasing, different types of exception handling

---
+++ exceptions/Exceptions/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("exception handling");
+        Console.WriteLine("------------------");
+
+        Console.WriteLine("Enter number:");
+        int n1=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter number:");
+        int n2=Convert.ToInt32(Console.ReadLine());
+
+        try
+        {
+            int result = n1 / n2;
+            Console.WriteLine("Result:" + result);
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine("Divide by zero not possible");
+            Console.WriteLine(e.Message);
+        }
+        Console.WriteLine();
+
+        int[] num = new int[3];
+
+        try
+        {
+            num[0] = 10;
+            num[1] = 20;
+            num[2] = 30;
+            num[3] = 40;
+
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        catch(IndexOutOfRangeException)
+        {
+            Console.WriteLine("Array range exceeded");
+        }
+        Console.WriteLine();
+
+        try
+        {
+            string text = null;
+            Console.WriteLine("Length:" + text.Length);
+        }
+        catch (NullReferenceException)
+        {
+            Console.WriteLine("Null cannot have length");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Enter something:");
+        string x=Console.ReadLine();
+
+        try
+        {
+            int x1 = int.Parse(x);
+            Console.WriteLine("Number:" + x1);
+        }
+        catch (FormatException) 
+        {
+            Console.WriteLine("String format not supported");
+        }
+        Console.WriteLine();
+
+    }
+}
---
+++ methods/Namespace/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+/* using aliasing to resolve when class have same name */
+using ss1 = sample.sub_sample;
+using ss2 = sample.sub_sample_s;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //sample.sub_sample.Test.display();
+        //sample.sub_sample_s.Test.display();
+
+        //direct call through 'using' and aliasing
+        ss1.Test.display();
+        ss2.Test.display();
+
+    }
+}
+
+namespace sample
+{
+    namespace sub_sample
+    {
+        class Test
+        {
+            public static void display()
+            {
+                Console.WriteLine("This is display method for 'Test' class");
+            }
+        }
+    }
+}
+
+namespace sample
+{
+    namespace sub_sample_s
+    {
+        class Test
+        {
+            public static void display()
+            {
+                Console.WriteLine("This is display method for 'sub_sample_s' namespace");
+            }
+        }
+    }
+}
