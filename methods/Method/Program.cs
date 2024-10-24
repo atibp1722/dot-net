4eb2c229bf1e39035c781d8af4444b0c2adb55e9
Atib Pokharel <atibp1722@gmail.com>
2024-10-24 12:49:13 +0545

    add file related to method; static and instance method, differeent type of method parameters

---
+++ methods/Method/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //instance method called by instance of the class
+        Program prg = new Program();
+        prg.numbers();
+
+        //static method called by class name itself
+        Program.number();
+
+        //calling parameterized method
+        Program prg1 = new Program();
+        prg1.nums(15);
+
+        Program prg2 = new Program();
+        Console.WriteLine(prg2.add(10, 20));
+    }
+
+    //instance method
+    public void numbers()
+    {
+        int f = 0;
+        while (f <= 10)
+        {
+            Console.Write(f+" ");
+            f += 2;
+        }
+        Console.WriteLine();
+    }
+
+    //static method
+    public static void number()
+    {
+        int f = 1;
+        while (f <= 10)
+        {
+            Console.Write(f+" ");
+            f += 2;
+        }
+    }
+
+    //parameterized method
+    public void nums(int n)
+    {
+        int f = 1;
+        while (f <= n)
+        {
+            Console.Write(f + " ");
+            f += 2;
+        }
+        Console.WriteLine();
+    }
+
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ methods/MethodParams/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int y = 10;
+        static_method(y);
+
+        int b = 10;
+        static_method(ref b);
+
+        Console.WriteLine(y);
+        Console.WriteLine(b);
+
+        int total = 0;
+        int product = 0;
+        calculator(7,8,out total,out product);
+        Console.WriteLine("Total: "+total);
+        Console.WriteLine("Product: "+product);
+
+        int[] num = new int[3];
+        num[0] = 17;
+        num[0] = 11;
+        num[0] = 10;
+        param_array(num);
+    }
+
+    //call by value- point at different location, modifying one does not affect value of another
+    public static void static_method(int x)
+    {
+        x= 1;
+    }
+
+    //call by reference- point at same location, modifying one does affect value of another
+    public static void static_method(ref int a)
+    {
+        a = 1;
+    }
+
+    //out params- multiple operations from same method
+    public static void calculator(int c,int d, out int add, out int multiply)
+    {
+        add = c + d;
+        multiply = c * d;
+    }
+
+    //parameter array- array has to be last parameter
+    public static void param_array(params int[] num)
+    {
+        Console.WriteLine("No of elements: "+num.Length);
+        foreach (int m in num)
+        {
+            Console.WriteLine(m);
+        }
+    }
+}
