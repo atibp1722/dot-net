4ea20cb57a923236b60952414a5ff316dee798f6
Atib Pokharel <atibp1722@gmail.com>
2024-10-27 12:54:52 +0545

    add file related to inheritance; method hidning

---
+++ inheritance/MethodHiding/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Worker
+{
+    public int id;
+    public string fname;
+    public string lname;
+
+    public void FullName()
+    {
+        Console.WriteLine("Full name: "+fname+" "+lname);
+    }
+}
+
+public class RemoteWorker: Worker
+{
+    public new void FullName()
+    {
+        Console.WriteLine("Full name: " + fname + " " + lname+" works virutally from Nepal");
+        //base.FullName();
+;    }
+}
+
+public class SiteWorker : Worker
+{
+
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        RemoteWorker rw = new RemoteWorker();
+        rw.id = 100;
+        rw.fname = "Ram";
+        rw.lname = "Karki";
+        rw.FullName();
+
+        SiteWorker sw = new SiteWorker();
+        sw.id = 110;
+        sw.fname = "Gita";
+        sw.lname = "Sharma";
+        sw.FullName();
+    }
+}
