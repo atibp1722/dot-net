3e3284fd26115c02a75e932e5fe4041febaaa7ad
Atib Pokharel <atibp1722@gmail.com>
2024-10-27 12:30:11 +0545

    add file related to inheritance; fundamental examples realted to vrious types of inheritance

---
+++ inheritance/Inheritance/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class Student
+{
+    public int id;
+    public string name;
+    public string course;
+    public int enroll_year;
+
+    public void StudentDetails()
+    {
+        Console.WriteLine(name+", "+course);
+    }
+
+}
+
+public class WorkingStudent: Student
+{
+    public string company;
+    public string status;
+    public float salary;
+
+    public void WorkingDetail()
+    {
+        Console.WriteLine(company+" "+status+" "+salary);
+    }
+}
+
+public class FullTimeStudent: Student
+{
+    public string status;
+    public float tuition;
+
+    public void FullTimeDetail()
+    {
+        Console.WriteLine(status+" "+ tuition);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FullTimeStudent fts = new FullTimeStudent();
+        fts.id = 100;
+        fts.name = "Ram Karki";
+        fts.course = "mathematics";
+        fts.enroll_year = 2022;
+        fts.status = "student";
+        fts.tuition = 8500.125f;
+        fts.StudentDetails();
+        fts.FullTimeDetail();
+
+        WorkingStudent ws = new WorkingStudent();
+        ws.id = 110;
+        ws.name = "Geeta Sharma";
+        ws.course = "economics";
+        ws.enroll_year = 2021;
+        ws.status = "working";
+        ws.company = "SCB";
+        ws.salary = 28000;
+        ws.StudentDetails();
+        ws.WorkingDetail();
+    }
+}
---
+++ inheritance/InheritanceType/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+//single inheritance
+public class Parent
+{
+    public Parent()
+    {
+        Console.WriteLine("This is parent class");
+    }
+
+    public Parent(string greet)
+    {
+        Console.WriteLine(greet);
+    }
+}
+
+public class Child : Parent
+{
+    public Child() : base("Parent from child class")
+    {
+        Console.WriteLine("This is child class");
+    }
+
+}
+
+//heirarchial inheritance
+public class Parents
+{
+    public Parents()
+    {
+        Console.WriteLine("parent called");
+    }
+}
+
+public class Son : Parents
+{
+    public Son()
+    {
+        Console.WriteLine("Son class called");
+    }
+}
+public class Daughter : Parents
+{
+    public Daughter()
+    {
+        Console.WriteLine("Daughter class called");
+    }
+}
+
+//multi-level inheritance
+public class Grandmothers
+{
+    public void base_method() 
+    {
+        Console.WriteLine("this is base class method");
+    }
+}
+
+public class Mothers: Grandmothers
+{
+    public void inter_method()
+    {
+        Console.WriteLine("intermediate class method");
+    }
+}
+
+public class Daughters: Mothers
+{
+    public void child_method()
+    {
+        Console.WriteLine("child class method");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Parent prt = new Parent();
+        Child child = new Child();
+        Console.WriteLine();
+
+        Son son = new Son();
+        Daughter dtr = new Daughter();
+        Console.WriteLine();
+
+        Daughters dtrs=new Daughters();
+        dtrs.base_method();
+        dtrs.inter_method();
+        dtrs.child_method();
+    }
+}
