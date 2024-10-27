d257d9a709dbbd36f5b0393540c889cf08bdab26
Atib Pokharel <atibp1722@gmail.com>
2024-10-27 13:41:51 +0545

    add file related to polymorphism; method overloading and method overriding

---
+++ polymorphism/Overload/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+
+class Program
+{
+    public void add()
+    {
+        int a=5, b=10;
+        int c = a + b;
+        Console.WriteLine(c);
+    }
+    public void add(int a, int b)
+    {
+        int c = a + b;
+        Console.WriteLine(c);
+    }
+    public void add(string a, string b)
+    {
+        string c = a+" "+b;
+        Console.WriteLine(c);
+    }
+    public void add(float a, float b)
+    {
+        float c = a + b;
+        Console.WriteLine(c);
+    }
+    public static void Main(string[] args)
+    {
+        Program prg = new Program();
+        prg.add();
+        prg.add(10, 20);
+        prg.add("hello", "world");
+        prg.add(5.65f, 2.75f);
+    }
+}
---
+++ polymorphism/Override/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void display()
+    {
+        Console.WriteLine("method of animal class");
+    }
+}
+class Dog: Animal
+{
+    public override void display()
+    {
+        Console.WriteLine("dog implementation of animal method");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.display();
+
+        Animal anm = new Dog();
+        anm.display();  
+    }
+}
