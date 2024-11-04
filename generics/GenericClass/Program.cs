4cf610baedd2e864e713f5340ef3a3db45773021
Atib Pokharel <atibp1722@gmail.com>
2024-11-04 12:58:25 +0545

    add generic file; example to demostrate generic method and generic class

---
+++ generics/GenericClass/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Test<X>
+{
+    X a;
+    
+    //public Test(X a)
+    //{
+    //    this.a = a;
+    //}
+
+    //public X getA()
+    //{
+    //    return this.a;
+    //}
+
+    public X getsA 
+    {
+        set 
+        {
+            this.a=value;
+        }
+        get 
+        {
+            return this.a;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Test<int> test = new Test<int>(15);
+        //Console.WriteLine(test.getA());
+
+        //Test<string> test1 = new Test<string>("Ram");
+        //Console.WriteLine(test1.getA());
+
+        //Test<int> test2 = new Test<int>();
+        //Console.WriteLine(test2.getA());
+
+        Test<char> test3 = new Test<char>();
+        test3.getsA = 'X';
+        Console.WriteLine(test3.getsA);
+
+        Test<double> test4 = new Test<double>();
+        test4.getsA = 3.75;
+        Console.WriteLine(test4.getsA);
+    }
+}
---
+++ generics/Generics/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+
+class Test
+{
+    public static void printArray<X>(X[] array) 
+    {
+        for (int i = 0; i < array.Length; i++) 
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+
+    public static bool checkStatus<Y>(Y a, Y b)
+    {
+        bool c =a.Equals(b);
+        return c;
+    }
+
+    public static void showType<Z>(Z c)
+    {
+        Console.WriteLine(typeof(Z));
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] num = new int[3];
+        num[0] = 10;
+        num[1] = 20;
+        num[2] = 30;
+
+        string[] text = { "java", "php", "c++" };
+
+        double[] point = { 1.5, 2.5, 3.5 };
+
+        Test.printArray(num);
+        Test.printArray(text);
+        Test.printArray(point);
+
+        Console.WriteLine(Test.checkStatus(1, 1));
+        Console.WriteLine(Test.checkStatus('a', 'A'));
+        Console.WriteLine(Test.checkStatus("ram", "ram"));
+        Test.showType("a");
+    }
+}
