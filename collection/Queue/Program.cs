66d7452262ac6433e0ef27b806da5601a67b4fca
Atib Pokharel <atibp1722@gmail.com>
2024-11-05 14:35:48 +0545

    add file related to collection; stack and queue

---
+++ collection/Queue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue queue = new Queue();
+        queue.Enqueue(10);
+        queue.Enqueue(2.5);
+        queue.Enqueue("c#");
+        queue.Enqueue('b');
+        queue.Enqueue(-1.5);
+
+        Console.WriteLine("Count: "+queue.Count);
+
+        foreach (var item in queue)
+        {
+            Console.Write(item+" ");
+        }
+        Console.WriteLine();
+
+
+        string a = queue.Peek().ToString();
+        Console.WriteLine("Peek: "+a);
+
+        //queue.Dequeue();
+        //queue.Dequeue();
+        //queue.Dequeue();
+
+        while (queue.Count > 0)
+        {
+            Console.WriteLine(queue.Dequeue());
+        }
+
+        Console.WriteLine("Count: " + queue.Count);
+    }
+}
---
+++ collection/Stacks/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main(string[] args) 
+    {
+        Stack stack = new Stack();
+        stack.Push(1);
+        stack.Push("hi");
+        stack.Push(2.5);
+        stack.Push('a');
+        stack.Push(-0.25);
+        stack.Push(1);
+        stack.Push(null);
+
+        Console.WriteLine(stack.Count);
+        Console.WriteLine(stack.Contains(1));
+
+        foreach (var item in stack) 
+        {
+            Console.Write(item+" ");
+        }
+        Console.WriteLine();
+
+        string a = stack.Peek().ToString();
+        Console.WriteLine(a);
+
+        stack.Pop();
+        stack.Pop();
+        stack.Pop();
+
+        foreach (var item in stack)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine(stack.Count);
+
+        Console.WriteLine(stack.Contains(1));
+    }
+}
