a31ea7bc4ea2d85c7ea950047b146ce58eebccd2
Atib Pokharel <atibp1722@gmail.com>
2024-11-05 13:28:41 +0545

    add file related to collection; list and arraylist example

---
+++ collection/Collection/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        ArrayList al = new ArrayList();
+        Console.WriteLine(al.Capacity);
+        al.Add(1);
+        al.Add("Ram");
+        al.Add('b');
+        al.Add(2.5);
+        Console.WriteLine(al.Capacity);
+        al.Add(-1);
+        Console.WriteLine(al.Capacity);
+
+        foreach (object i in al) 
+        {
+            Console.Write(i+" ");
+        }
+        Console.WriteLine();
+
+        al.Insert(3, "new#1");
+        al.Insert(4, "new#2");
+
+        foreach (object i in al)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+
+        al.RemoveAt(3);
+        al.Remove("new#2");
+
+        foreach (object i in al)
+        {
+            Console.Write(i + " ");
+        }
+
+        //int[] num = new int[3] { 1, 2, 3 };
+        //for (int i = 0; i < num.Length; i++)
+        //{
+        //    Console.WriteLine(num[i]);
+        //}
+        //Console.WriteLine("-----");
+
+        //Array.Resize(ref num, 4);
+        //num[3] = 4;
+
+        //for (int i = 0; i < num.Length; i++)
+        //{
+        //    Console.WriteLine(num[i]);
+        //}
+    }
+}
---
+++ collection/Lists/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> name = new List<string>();
+        name.Add("Ram");
+        name.Add("Geeta");
+
+        List<string> new_name = new List<string>();
+        new_name.Add("Nita");
+        new_name.Add("Indra");
+
+        name.AddRange(new_name);
+
+        for (int i = 0; i < name.Count; i++) 
+        {
+            var item= name[i];
+            Console.Write(item+" ");
+        }
+        Console.WriteLine();
+
+        foreach (var i in name) 
+        {
+            Console.Write(i+" ");
+        }
+        Console.WriteLine();
+
+        List<string> text = new List<string>
+        {
+            "java",
+            "php",
+            ".net",
+            "c++"
+        };
+        text.Add("js");
+        text.Insert(1, "python");
+        text.InsertRange(2,new_name);
+
+        foreach (var i in text)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine(text.Contains("java"));
+
+        text.RemoveRange(2,3);
+
+        foreach (var i in text)
+        {
+            Console.Write(i + " ");
+        }
+
+    }
+}
