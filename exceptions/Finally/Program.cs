94a77cb41a8d2e92dea41643adbebe2c7fb17fce
Atib Pokharel <atibp1722@gmail.com>
2024-11-11 12:53:52 +0545

    add basic example to demonstrate finally in exception handling

---
+++ exceptions/Finally/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int a = 2;
+            int b = 0;
+            int c = a / b;
+            Console.WriteLine("Result {0}", c);
+
+            string text = null;
+            Console.WriteLine(text.Length);
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (NullReferenceException e) 
+        { 
+            Console.WriteLine(e.Message); 
+        }
+        catch (FileNotFoundException e) 
+        { 
+            Console.WriteLine(e.Message); 
+        }
+        finally
+        {
+            Console.WriteLine("finally block reached");
+        }
+    }
+}
