cea42861a31b0d4a0da5d64e3d68f6836ed8fb3e
Atib Pokharel <atibp1722@gmail.com>
2024-11-12 14:16:21 +0545

    add file related to file handling; example of directory handling and its methods

---
+++ flies/Directory/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        string path = @"C:\\Users\\Admin\\Desktop\\test_directory";
+        //string path1 = @"C:\\Users\\Admin\\Desktop\\test_directory_1";
+
+        DirectoryInfo di = new DirectoryInfo(path);
+        di.Create();
+        di.CreateSubdirectory("sub_directory");
+
+        //move to shifts all to new and deletes the old directory
+        //di.MoveTo(path1);
+
+        //delete empty directory only
+        //di.Delete(true);
+
+        Console.WriteLine(di.Name);
+        Console.WriteLine(di.FullName);
+        Console.WriteLine(di.LastAccessTime);
+        Console.WriteLine(di.Parent);
+        Console.WriteLine(di.Root);
+        Console.WriteLine(di.LastWriteTime);
+
+        DirectoryInfo[] dir_list = di.GetDirectories(path);
+        foreach(var i in dir_list) 
+        {
+            Console.WriteLine(i);
+        }
+
+        foreach (var i in dir_list)
+        {
+            Console.WriteLine(i.GetFiles().Length);
+        }
+
+        //Console.WriteLine("directory deleted");
+    }
+}
