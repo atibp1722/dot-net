f5ab34176cd75977648b66f83d9926ad73a837d7
Atib Pokharel <atibp1722@gmail.com>
2024-11-12 12:36:43 +0545

    add file related to file handling; example files of stream writer and reader

---
+++ flies/Stream/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        string path = @"C:\\Users\\Admin\\Desktop\\test\\text.txt";
+
+        using FileStream fs = new(path,FileMode.Append,FileAccess.Write);
+        {
+            using StreamWriter sw = new StreamWriter(fs);
+            {
+                sw.WriteLine("stream writer");
+                sw.WriteLine("file handling");
+
+                int[] num = { 0, 1, 2, 3 };
+                foreach (int i in num)
+                {
+                    sw.Write(i+" ");
+                }
+            }            
+        }
+    }
+}
---
+++ flies/Stream/StrmReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stream
+{
+    public class StrmReader
+    {
+        public void Main(string[] args)
+        {
+            string path = @"C:\\Users\\Admin\\Desktop\\test\\text.txt";
+            using FileStream fs = new(path, FileMode.Open, FileAccess.Read);
+            {
+                using StreamReader sr = new StreamReader(fs);
+                {
+                    foreach(var line in sr.ReadLine())
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    string lines = "";
+                    while ((lines = sr.ReadLine()) != null) 
+                    {
+                        Console.WriteLine(lines);
+                    }
+
+                    string line1 = sr.ReadToEnd();
+                    Console.WriteLine(line1);
+
+                    if (sr.Peek() > 0)
+                    {
+                        Console.WriteLine("yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("no");
+                    }
+                }
+            }
+        }
+    }
+}
