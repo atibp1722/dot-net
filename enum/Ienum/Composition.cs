51937148b294adbf56e2d129b561bd0356d4a2a8
Atib Pokharel <atibp1722@gmail.com>
2024-11-13 14:30:27 +0545

    add enumeration file; exampel to demonstrate ienumeration and ienumerable using generics

---
+++ enum/Ienum/Composition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ienum
+{
+    class Composition<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator() 
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() 
+        {
+            throw new NotImplementedException ();
+        }
+    }
+}
---
+++ enum/Ienum/Program.cs
@@ -0,0 +1,32 @@
+﻿using Ienum;
+using System;
+using System.Linq;
+
+class Program
+{
+    public static bool isEven(int num)
+    {
+        return num % 2 == 0;
+    }
+    static void Main(string[] args)
+    {
+        var num1 = new List<int> {1,2,3,4,5};
+        var num2 = new List<int> {5,6,7,8,9};
+        var num3 = new HashSet<int> {3,6,9,12,15};
+        var num4 = new [] {5,10,15,20,25};
+
+        //var cmp = new Composition<int> {num1,num2,num3,num4 };
+
+        //int evenCnt = 0;
+
+        //foreach (var num in cmp) 
+        //{
+        //    if (isEven(num)) 
+        //    {
+        //        evenCnt++;
+        //    }
+        //}
+
+        //int evenCntr = cmp.Count(x => isEven(x));
+    }
+}
